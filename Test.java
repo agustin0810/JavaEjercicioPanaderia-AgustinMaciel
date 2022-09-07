0c7c1864544b5b94bdf8894d5d7987cd694f6133
Agustin Maciel <agustinmaciel0810@gmail.com>
2022-09-07 20:56:21 -0300

    Initial push

---
+++ Test.java
@@ -0,0 +1,10 @@
+package ClasesDDA.cerosieteseptiembre;
+
+public class Test {
+    public static void mostrarTexto(){
+        System.out.println("Texto");
+    }
+    public boolean prueba(){
+        return true;
+    }
+}
---
+++ cerosieteceronueve.java
@@ -0,0 +1,9 @@
+package ClasesDDA.cerosieteseptiembre;
+
+public class cerosieteceronueve {
+    public static void main(String[] args){
+        Test.mostrarTexto();
+        boolean resp = new Test().prueba();
+        System.out.println(resp);
+    }
+}
